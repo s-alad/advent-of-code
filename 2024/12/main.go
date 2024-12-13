f50d51b40ebd8e5585c8fe6562a3abeb682fdfc8
s-alad <n7.saad@gmail.com>
2024-12-13 02:21:12 -0500

    slop 12

--- 2024/12/main.go
+++ 2024/12/main.go
@@ -34,6 +34,10 @@ var directions = []Vector{
 	{Coordinate{0, -1}, Left},
 }
 
+func unbounded(c Coordinate, graph [][]string) bool {
+	return c.x < 0 || c.y < 0 || c.x >= len(graph) || c.y >= len(graph[0])
+}
+
 func boundry(c Coordinate, graph [][]string) bool {
 	return c.x >= 0 && c.y >= 0 && c.x < len(graph) && c.y < len(graph[0])
 }
@@ -74,6 +78,124 @@ func one(graph [][]string) {
 	fmt.Println(price)
 }
 
+func flow(graph [][]string, shape mapset.Set[Coordinate]) int {
+	perimeter := 0
+	land := make([][]string, len(graph))
+	for i := range graph {
+		land[i] = make([]string, len(graph[i]))
+		for j := range land[i] {
+			land[i][j] = "#"
+		}
+	}
+	for _, p := range shape.ToSlice() {
+		land[p.x][p.y] = "X"
+	}
+
+	for _, direction := range directions {
+		switch direction.direction {
+		case Up:
+			startX := len(land)
+			startY := 0
+
+			for start := startX; start > 0; start-- {
+				neighbors := mapset.NewSet[Coordinate]()
+				for i := startY; i < len(land[0]); i++ {
+					if (unbounded(Coordinate{start, i}, land) || land[start][i] == "#") && land[start-1][i] == "X" {
+						valid := true
+						for _, n := range neighbors.ToSlice() {
+							if n.x == start && (n.y == i-1 || n.y == i+1) {
+								valid = false
+								break
+							}
+						}
+
+						if valid {
+							perimeter++
+						}
+
+						neighbors.Add(Coordinate{start, i})
+					}
+				}
+			}
+
+		case Down:
+			startX := -1
+			startY := 0
+
+			for start := startX; start < len(land)-1; start++ {
+				neighbors := mapset.NewSet[Coordinate]()
+				for i := startY; i < len(land[0]); i++ {
+					if (unbounded(Coordinate{start, i}, land) || land[start][i] == "#") && land[start+1][i] == "X" {
+						valid := true
+						for _, n := range neighbors.ToSlice() {
+							if n.x == start && (n.y == i-1 || n.y == i+1) {
+								valid = false
+								break
+							}
+						}
+
+						if valid {
+							perimeter++
+						}
+
+						neighbors.Add(Coordinate{start, i})
+					}
+				}
+			}
+		case Left:
+			startY := -1
+			startX := 0
+
+			for start := startY; start < len(land[0])-1; start++ {
+				neighbors := mapset.NewSet[Coordinate]()
+				for i := startX; i < len(land); i++ {
+					if (unbounded(Coordinate{i, start}, land) || land[i][start] == "#") && land[i][start+1] == "X" {
+						valid := true
+						for _, n := range neighbors.ToSlice() {
+							if n.y == start && (n.x == i-1 || n.x == i+1) {
+								valid = false
+								break
+							}
+						}
+
+						if valid {
+							perimeter++
+						}
+
+						neighbors.Add(Coordinate{i, start})
+					}
+				}
+			}
+		case Right:
+			startY := len(land[0])
+			startX := 0
+
+			for start := startY; start > 0; start-- {
+				neighbors := mapset.NewSet[Coordinate]()
+				for i := startX; i < len(land); i++ {
+					if (unbounded(Coordinate{i, start}, land) || land[i][start] == "#") && land[i][start-1] == "X" {
+						valid := true
+						for _, n := range neighbors.ToSlice() {
+							if n.y == start && (n.x == i-1 || n.x == i+1) {
+								valid = false
+								break
+							}
+						}
+
+						if valid {
+							perimeter++
+						}
+
+						neighbors.Add(Coordinate{i, start})
+					}
+				}
+			}
+		}
+	}
+
+	return perimeter
+}
+
 func two(graph [][]string) {
 	visited := mapset.NewSet[Coordinate]()
 	price := 0
@@ -84,79 +206,24 @@ func two(graph [][]string) {
 			if visited.Contains(point) {
 				continue
 			} else {
-				area := 0
-				perimeter := 0
-				vectors := mapset.NewSet[Vector]()
+				area := 1
+				shape := mapset.NewSet[Coordinate]()
 
-				bfs := func(start Coordinate, graph [][]string, crop string) {
-					queue := []Coordinate{start}
-					visited.Add(start)
-
-					for len(queue) > 0 {
-						current := queue[0]
-						queue = queue[1:]
-						area++
-
-						for _, direction := range directions {
-							nv := Coordinate{current.x + direction.coordinate.x, current.y + direction.coordinate.y}
-							if boundry(nv, graph) && !visited.Contains(nv) && graph[nv.x][nv.y] == crop {
-								queue = append(queue, nv)
-								visited.Add(nv)
-							} else if !boundry(nv, graph) || graph[nv.x][nv.y] != crop {
-								orientation := direction.direction
-								valid := true
-								switch orientation {
-								case Up:
-									for _, v := range vectors.ToSlice() {
-										if v.direction == Up &&
-											(v.coordinate.y == nv.y-1 || v.coordinate.y == nv.y+1) &&
-											(v.coordinate.x == nv.x) {
-											valid = false
-										}
-									}
-								case Down:
-									for _, v := range vectors.ToSlice() {
-										if v.direction == Down &&
-											(v.coordinate.y == nv.y-1 || v.coordinate.y == nv.y+1) &&
-											(v.coordinate.x == nv.x) {
-											valid = false
-										}
-									}
-								case Left:
-									for _, v := range vectors.ToSlice() {
-										if v.direction == Left &&
-											(v.coordinate.x == nv.x-1 || v.coordinate.x == nv.x+1) &&
-											(v.coordinate.y == nv.y) {
-											valid = false
-										}
-									}
-								case Right:
-									for _, v := range vectors.ToSlice() {
-										if v.direction == Right &&
-											(v.coordinate.x == nv.x-1 || v.coordinate.x == nv.x+1) &&
-											(v.coordinate.y == nv.y) {
-											valid = false
-										}
-									}
-								}
-
-								if valid {
-									perimeter++
-								}
-
-								vectors.Add(Vector{
-									Coordinate{nv.x, nv.y},
-									orientation,
-								})
-							}
+				var dfs func(vertex Coordinate, graph [][]string, crop string)
+				dfs = func(vertex Coordinate, graph [][]string, crop string) {
+					visited.Add(vertex)
+					shape.Add(vertex)
+					for _, direction := range directions {
+						nv := Coordinate{vertex.x + direction.coordinate.x, vertex.y + direction.coordinate.y}
+						if boundry(nv, graph) && !visited.Contains(nv) && graph[nv.x][nv.y] == crop {
+							area++
+							dfs(nv, graph, crop)
 						}
 					}
 				}
-
 				crop := graph[x][y]
-				//fmt.Println("starting with", crop)
-				bfs(point, graph, crop)
-				//fmt.Printf("ended with %d area and %d perimeter for %s\n", area, perimeter, crop)
+				dfs(point, graph, crop)
+				perimeter := flow(graph, shape)
 				price += area * perimeter
 			}
 		}
@@ -169,13 +236,13 @@ func main() {
 	data, _ := os.ReadFile("data.txt")
 	graph := lo.Map(strings.Split(string(data), "\n"), func(line string, _ int) []string { return strings.Split(line, "") })
 
-	for _, row := range graph {
+	/* for _, row := range graph {
 		for _, cell := range row {
 			fmt.Print(cell)
 		}
 		fmt.Println()
-	}
+	} */
 
-	//one(graph)
+	one(graph)
 	two(graph)
 }
